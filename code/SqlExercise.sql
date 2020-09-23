971d39dda1a897c862bd713d932707b1fbcb03d0
jobymailbox <71743857+jobymailbox@users.noreply.github.com>
2020-09-23 11:38:06 -0400

    Update SqlExercise.sql
    added the create temp# , insert and select statement

--- code/SqlExercise.sql
+++ code/SqlExercise.sql
@@ -1,5 +1,22 @@
+
+IF OBJECT_ID('tempdb..#tempresult') IS NOT NULL
+	DROP TABLE #tempresult;
+create table #tempresult
+(
+Id INT,
+PatientId INT,
+TestType varchar(10),
+EffectiveDateTime varchar(50),
+Value INT
+);
+BULK INSERT #tempresult
+FROM 'C:\Users\1146595\TEST\csv\TSV\test_result.tsv'
+WITH (
+  FIELDTERMINATOR = '\t',
+  FIRSTROW = 2
+);
 SELECT MAX(EffectiveDateTime) AS EffectiveDateTime,Patientid,TestType FROM 
-TEST_RESULT
+#tempresult
 WHERE VALUE >7
-AND TestType ='a1c'
+AND TESTTYPE ='a1c'
 GROUP BY Patientid,TestType
