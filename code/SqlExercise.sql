9b1c5d5d77b53852cd9eb16a1d7ae925fd280809
jobymailbox <71743857+jobymailbox@users.noreply.github.com>
2020-09-25 08:31:55 -0400

    Added commets

--- code/SqlExercise.sql
+++ code/SqlExercise.sql
@@ -1,3 +1,4 @@
+--temp table creation
 
 IF OBJECT_ID('tempdb..#tempresult') IS NOT NULL
 	DROP TABLE #tempresult;
@@ -9,12 +10,16 @@ TestType varchar(10),
 EffectiveDateTime varchar(50),
 Value INT
 );
+-- Insert data from tsv file to temp table
+
 BULK INSERT #tempresult
-FROM 'C:\Users\1146595\TEST\csv\TSV\test_result.tsv'
+FROM 'C:\Users\JJ\TSV\test_result.tsv'
 WITH (
   FIELDTERMINATOR = '\t',
   FIRSTROW = 2
 );
+-- Select the latest records based on effective date from test type 'a1c'
+
 SELECT MAX(EffectiveDateTime) AS EffectiveDateTime,Patientid,TestType FROM 
 #tempresult
 WHERE VALUE >7
